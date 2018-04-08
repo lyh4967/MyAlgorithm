898b23b490eb41995e87d62c4c21ad38fcb87920
lyh4967 <lyh4965@naver.com>
2018-04-08 14:44:31 +0900

    2667

---
+++ MyAlgorithm/MyAlgorithm/[10451]permutation_cycle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(int start, vector<int> *graph, bool *check, int& cnt){
+	if (check[start]){//이미 방문했던곳이라면 리턴
+		return;
+	}
+	check[start] = true;
+	int next = graph[start][0]; // 다음 방문장소 지정
+	if (!check[next])
+		dfs(next, graph, check, cnt);
+	else
+		cnt++;// 다음방문장소가 체크되어있다면 카운트++
+}
+
+int main(){
+	int testNum, nodeNum;
+	cin >> testNum;
+
+	while (testNum > 0){
+		cin >> nodeNum;
+		bool *checkArr = new bool[nodeNum + 1];
+		fill(checkArr, checkArr + nodeNum + 1, false);
+		vector<int> *graphList = new vector<int>[nodeNum + 1];
+		for (int i = 1; i <= nodeNum; i++){
+			int tmp;
+			cin >> tmp;
+			graphList[i].push_back(tmp);
+		}
+		int cnt = 0;
+		for (int i = 1; i <= nodeNum; i++)
+			dfs(i, graphList, checkArr, cnt);
+		cout << cnt << endl;
+		testNum--;
+	}
+}
--- MyAlgorithm/MyAlgorithm/testDriver.cpp
+++ MyAlgorithm/MyAlgorithm/testDriver.cpp
@@ -1,37 +1,70 @@
-#include <iostream>
+#include<iostream>
+#include <algorithm>
+#include <string>
+#include <stdio.h>
 #include <vector>
-using namespace std;
+#pragma warning(disable:4996)
 
-void dfs(int start, vector<int> *graph, bool *check,int& cnt){
-	if (check[start]){//이미 방문했던곳이라면 리턴
-		return;
-	}
-	check[start] = true;
-	int next = graph[start][0]; // 다음 방문장소 지정
-	if (!check[next])
-		dfs(next, graph, check, cnt);
-	else
-		cnt++;// 다음방문장소가 체크되어있다면 카운트++
+using namespace std;
+int N;//지도의 크기
+int cnt;
+
+bool testBoundry(int a, int b){
+	return ((a < N&&a >= 0) && (b < N&&b >= 0));
 }
 
-int main(){
-	int testNum, nodeNum;
-	cin >> testNum;
+int dirArr[4][2] = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };//탐색할방향지정
 
-	while (testNum > 0){
-		cin >> nodeNum;   
-		bool *checkArr = new bool[nodeNum + 1];
-		fill(checkArr, checkArr + nodeNum + 1, false);
-		vector<int> *graphList = new vector<int>[nodeNum+1];
-		for (int i = 1; i <= nodeNum; i++){
-			int tmp;
-			cin >> tmp;
-			graphList[i].push_back(tmp);
-		}
-		int cnt = 0; 
-		for (int i = 1; i <= nodeNum; i++)
-			dfs(i, graphList, checkArr, cnt);
-		cout << cnt << endl;
-		testNum--;
+void dfs(int **graphArr,bool **checkArr, int a,int b){
+	cnt++;//방문시 카운트
+	checkArr[a][b] = true;//방문시 체크한다.
+	for (int i = 0; i < 4; i++){//4방향 모두 탐색한다.
+		int x = a + dirArr[i][0];
+		int y = b + dirArr[i][1];
+		if (testBoundry(x, y)&&(checkArr[x][y] == false) && (graphArr[x][y] == 1))
+			dfs(graphArr, checkArr, x, y);
 	}
+}
+
+
+int main(){
+
+	cin >> N;
+	int **graphArr = new int*[N];
+	for (int i = 0; i < N; i++)
+		graphArr[i] = new int[N];
+	
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			scanf("%1d", &graphArr[i][j]);
+		}
+	}
+
+	bool **checkArr = new bool*[N];
+	for (int i = 0; i < N; i++){
+		checkArr[i] = new bool[N];
+	}
+
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			checkArr[i][j] = false;
+		}
+	}
+
+	vector<int> cntVector;
+
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			if ((graphArr[i][j] == 1)&&(checkArr[i][j]==false)){
+				cnt = 0;
+				dfs(graphArr, checkArr, i, j);
+				cntVector.push_back(cnt);
+			}
+		}
+	}
+	sort(cntVector.begin(), cntVector.end());
+	cout << cntVector.size() << endl;
+	for (int i = 0; i < (int)cntVector.size(); i++)
+		cout << cntVector[i] << endl;
+	return 0;
 }
