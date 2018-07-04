5328752d29d8290691b129106f43fd93572d30c9
이영훈 <2012103993>
2018-07-04 14:33:17 +0900

    start vacation

--- MyAlgorithm/MyAlgorithm/testDriver.cpp
+++ MyAlgorithm/MyAlgorithm/testDriver.cpp
@@ -1,24 +1,9 @@
 ﻿#include <iostream>
-#include <string>
 using namespace std;
-
-
 
 int main() {
 
-	string document,word;
-	getline(cin,document);
-	getline(cin,word);
-	int index = 0;
-	int cnt = 0;
-	while (document.find(word,index) <= 2500) {
-		cnt++;
-		index = document.find(word, index)+word.size();
-	}
-	cout << cnt << endl;
-
-
 	
-	return 0;
 
+	return 0;
 }
