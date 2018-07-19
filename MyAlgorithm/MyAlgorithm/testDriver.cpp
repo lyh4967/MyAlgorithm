c29efef893aaaa6348b3c081aedf46d83b20f6e8
이영훈 <2012103993>
2018-07-19 14:51:37 +0900

    2018-07-19

--- MyAlgorithm/MyAlgorithm/testDriver.cpp
+++ MyAlgorithm/MyAlgorithm/testDriver.cpp
@@ -1,43 +1,102 @@
 ﻿#include <iostream>
 using namespace std;
 
+int** map;
+long long minM= 9223372036854775806;
+int n, wait;
+int flag;
+
+
+bool boundry(int a, int b) {
+	return (0 <= a && a < n) && (0 <= b && b < n);
+}
+
+void dfs(int a,int b,int cnt) {
+	if (cnt <= minM) {
+		if (a == n - 1 && b == n - 1) {
+			if (cnt < minM)
+				minM = cnt;
+			return;
+		}
+		if (boundry(a + 1, b)) {
+			if (map[a + 1][b] == 1) {
+				dfs(a + 1, b, cnt + 1);
+			}
+			else {
+				if (map[a + 1][b] == 0) {
+					if (cnt%wait == wait - 1) {
+						dfs(a + 1, b, cnt + 1);
+					}
+					else {
+						dfs(a, b, cnt + 1);
+					}
+				}
+				else {
+					if (cnt == 0) {
+						dfs(a + 1, b, cnt + 1);
+					}
+					else if (cnt == map[a + 1][b] - 1) {
+						dfs(a + 1, b, cnt + 1);
+					}
+					else {
+						dfs(a, b, cnt + 1);
+					}
+				}
+			}
+		}
+		if (boundry(a, b + 1)) {
+			if (map[a][b + 1] == 1) {
+				dfs(a, b + 1, cnt + 1);
+			}
+			else {
+				if (map[a][b + 1] == 0) {
+					if (cnt%wait == wait - 1) {
+						dfs(a, b + 1, cnt + 1);
+					}
+					else {
+						dfs(a, b, cnt + 1);
+					}
+				}
+				else {
+					if (cnt == 0) {
+						dfs(a, b+1, cnt + 1);
+					}
+					else if (cnt == map[a][b + 1] - 1) {
+						dfs(a, b + 1, cnt + 1);
+					}
+					else {
+						dfs(a, b, cnt + 1);
+					}
+				}
+			}
+		}
+	}
+		
+}
+
 int main() {
 
 	int testNum;
 	cin >> testNum;
 	for (int test = 1; test <= testNum; test++) {
-
-		int n;
-		cin >> n;
-		int arr[401];
-		fill(arr, arr + 401, 0);
+		
+		cin >> n >> wait;
+		map = new int*[n];
+		for (int i = 0; i < n; i++)
+			map[i] = new int[n];
+		
 		for (int i = 0; i < n; i++) {
-			int from, to;
-			cin >> from >> to;
-			if (from > to) {
-				int temp = from;
-				from = to;
-				to = temp;
-			}
-			for (int j = from; j <= to; j++) {
-				arr[j] += 1;
-			}
-			if (from % 2 == 0) {
-				arr[from - 1] += 1;
-			}
-			if (to % 2 == 1) {
-				arr[to + 1] += 1;
+			for (int j = 0; j < n; j++) {
+				cin >> map[i][j];
 			}
 		}
-		int max = 0;
-		for (int i = 1; i <= 400; i++) {
-			if (max < arr[i])
-				max = arr[i];
-		}
-		cout << "#" << test << ' ' << max << endl;
+		minM = 9223372036854775806;
+
+		dfs(0, 0, 0);
+		cout <<"#"<<test<<' '<< minM << endl;
+
 
 	}
 
 	return 0;
-
 }
