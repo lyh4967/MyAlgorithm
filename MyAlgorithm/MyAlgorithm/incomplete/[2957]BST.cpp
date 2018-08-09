d6295e7b3e8a1ac6c108af760dda87dd2917ee1d
이영훈 <2012103993>
2018-08-09 11:39:42 +0900

    [15686]

---
+++ MyAlgorithm/MyAlgorithm/incomplete/[2957]BST.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int C = 0;
+class Node {
+public:
+	Node() {}
+	Node(int _item) :item(_item) {}
+
+	int item;
+	Node* left;
+	Node* right;
+
+};
+
+void insert(int x, Node* n) {
+	if (n->item == NULL) {
+		n->item = x;
+	}
+	else if (x < n->item) {
+		C++;
+		if (n->left == NULL) {
+			Node* temp = new Node;
+			temp->item = x;
+			n->left = temp;
+		}
+		else
+			insert(x, n->left);
+	}
+	else {
+		C++;
+		if (n->right == NULL) {
+			Node* temp = new Node;
+			temp->item = x;
+			n->right = temp;
+		}
+		else
+			insert(x, n->right);
+	}
+}
+
+int main() {
+
+	int n; cin >> n;
+	Node* root = new Node;
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		insert(num, root);
+		cout << C << endl;
+	}
+
+	return 0;
+}
--- MyAlgorithm/MyAlgorithm/testDriver.cpp
+++ MyAlgorithm/MyAlgorithm/testDriver.cpp
@@ -1,53 +1,78 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-int C = 0;
-class Node {
-public:
-	Node() {}
-	Node(int _item) :item(_item) {}
-
-	int item;
-	Node* left;
-	Node* right;
-
+#define MAX_N 51
+#define MAX_M 14
+#define INF 987654321
+int n, m;
+int map[MAX_N][MAX_N];
+bool visited[MAX_M];
+int house_len;
+int chicken_len;
+int answer = INF;
+struct Pair {
+	int a;
+	int b;
+	long long cnt;
+	Pair() { cnt = 0; }
+	Pair(int _a, int _b) :a(_a), b(_b) { cnt = 0; }
 };
+vector<Pair> houses;
+vector<Pair> chickens;
 
-void insert(int x, Node* n) {
-	if (n->item == NULL) {
-		n->item = x;
-	}
-	else if (x < n->item) {
-		C++;
-		if (n->left == NULL) {
-			Node* temp = new Node;
-			temp->item = x;
-			n->left = temp;
+void dfs(int index, int c_cnt) {
+
+	if (index == chicken_len+1)
+		return;
+	if (c_cnt == m) {
+		/*cout << "log: " << c_cnt << endl;
+		for (int i = 0; i < chicken_len; i++)
+			cout << visited[i] << " ";
+		cout << endl;*/
+
+		int result = 0;
+		for (int i = 0; i < house_len; i++) {
+			int minDis = INF;
+			for (int j = 0; j < chicken_len; j++) {
+				if (visited[j] == true) {
+					int dis = abs(houses[i].a - chickens[j].a) + abs(houses[i].b - chickens[j].b);
+					if (dis < minDis)
+						minDis = dis;
+				}
+			}
+			result += minDis;
 		}
-		else
-			insert(x, n->left);
+		if (answer > result)
+			answer = result;
+		return;
 	}
-	else {
-		C++;
-		if (n->right == NULL) {
-			Node* temp = new Node;
-			temp->item = x;
-			n->right = temp;
-		}
-		else
-			insert(x, n->right);
-	}
+
+	visited[index] = true;
+	dfs(index + 1, c_cnt + 1);
+	visited[index] = false;
+	dfs(index + 1, c_cnt);
 }
-
 int main() {
-
-	int n; cin >> n;
-	Node* root = new Node;
-	for (int i = 0; i < n; i++) {
-		int num; cin >> num;
-		insert(num, root);
-		cout << C << endl;
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 1)
+				houses.push_back(Pair(i, j));
+			else if (map[i][j] == 2)
+				chickens.push_back(Pair(i, j));
+		}
 	}
+	house_len = houses.size();
+	chicken_len = chickens.size();
+	fill(visited, visited + MAX_M, false);
+
+	dfs(0, 0);
+	cout << answer << endl;
 
 	return 0;
 }
