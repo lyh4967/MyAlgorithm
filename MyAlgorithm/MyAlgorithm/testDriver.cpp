fb59c39f0c719d48ec064691b4d0920d618b1b12
이영훈 <2012103993>
2018-09-04 18:56:58 +0900

    6087

--- MyAlgorithm/MyAlgorithm/testDriver.cpp
+++ MyAlgorithm/MyAlgorithm/testDriver.cpp
@@ -1,86 +1,113 @@
 ﻿#include <iostream>
-#include <string>
 #include <queue>
 using namespace std;
 int dir[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
-int n, m, k;
-int answer = 0;
-int length;
+int min_m(int a, int b) {
+	return a > b ? b : a;
+}
+int max_m(int a, int b) {
+	return a > b ? a : b;
+}
+struct Node {
+	int a; int b; int cnt;
+};
+
 char map[100][100];
-int visited[100][100][81];
-string str;
+bool visited[100][100];
+int w, h;
+int sA, sB, eA, eB;
+int answer = 987654321;
+int length;
+
+bool check(int a, int b) {
+	int tmpA = eA;
+	int tmpB = eB;
+	bool flag = false;//벽이있는지검사
+	if (a == eA) {//같은 행
+		if (b > eB) {
+			swap(b, tmpB);
+		}
+		for (int i = b + 1; i < tmpB; i++)
+			if (map[a][i] == '*')
+				flag = true;
+	}
+	else {//같은 열
+		if (a > eA) {
+			swap(a, tmpA);
+		}
+		for (int i = a + 1; i < tmpA; i++)
+			if (map[i][b] == '*')
+				flag = true;
+	}
+	return flag;
+}
 
 bool isRanged(int a, int b) {
-	return 0 <= a && a < n && 0 <= b && b < m;
+	return 0 <= a && a < h && 0 <= b && b < w;
 }
-
-int dfs(int a, int b, int cnt) {
-	if (cnt == length - 1) 
-		return 1;
-
-	if (visited[a][b][cnt] != -1)
-		return visited[a][b][cnt];
-
-	visited[a][b][cnt] = 0;
-	for (int i = 0; i < 4; i++) {
-		for (int jump = 1; jump <= k; jump++) {
-			int nA = a + dir[i][0] * jump;
-			int nB = b + dir[i][1] * jump;
-
-			if (isRanged(nA, nB) && map[nA][nB] == str[cnt + 1]) {
-				visited[a][b][cnt] += dfs(nA, nB, cnt + 1);
-			}
-		}
-	}
-	return visited[a][b][cnt];
-}
-
-void bfs() {
-	//while (!que.empty()) {
-
-	//	node = que.front(); que.pop();
-	//	int a = node.a; int b = node.b; int cnt = node.cnt;
-	//	if (cnt == length - 1) {
-	//		answer++;
-	//		continue;
-	//	}
-
-	//	if (visited[a][b][cnt])
-	//		continue;
-
-	//	visited[a][b][cnt] = true;
-
-	//	for (int i = 0; i < 4; i++) {
-	//		for(int jump = 1;jump <= k; jump++){
-	//			int nA = a + dir[i][0] * jump;
-	//			int nB = b + dir[i][1] * jump;
-	//			//cout << "log: " << nA << "," << nB << "," << cnt << endl;
-
-	//			if (isRanged(nA, nB) && map[nA][nB] == str[cnt + 1] && !visited[nA][nB][cnt + 1]) {
-	//				node.a = nA; node.b = nB; node.cnt = cnt + 1;
-	//				que.push(node);
-	//			}
-	//		}
-	//	}
-	//}
+void swap(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
 }
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
-
-	cin >> n >> m >> k;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			fill(visited[i][j], visited[i][j] + 81, -1);
+	cin >> w >> h;
+	bool tmpFlag = false;
+	Node node;
+	queue<Node> que;
+	length = max_m(w, h);
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
 			cin >> map[i][j];
+			if (map[i][j] == 'C') {
+				if (tmpFlag == false) {
+					sA = i; sB = j;
+					node.a = sA; node.b = sB; node.cnt = 0;
+					tmpFlag = true;
+					que.push(node);
+				}
+				else {
+					eA = i; eB = j;
+				}
+			}
+			visited[i][j] = false;
 		}
 	}
-	cin >> str;
-	length = str.size();
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j] == str[0]) {
-				answer += dfs(i, j, 0);
+
+	while (!que.empty()) {
+		node = que.front(); que.pop();
+		int a = node.a; int b = node.b; int cnt = node.cnt;
+		if (cnt > answer)
+			continue;
+
+		if (a==eA || b==eB) {
+			
+			//cout << "end: " << eA << "," << eB << endl;
+			if (!check(a,b)) {
+				answer = min_m(answer, cnt);
+				continue;
+			}
+		}
+		
+		if (visited[a][b])
+			continue;
+		//cout << "log: " << a << "," << b << endl;
+
+		visited[a][b] = true;
+		for (int i = 0; i < 4; i++) {
+			for (int jump = 1; jump < length; jump++) {
+				int nA = a + dir[i][0] * jump;
+				int nB = b + dir[i][1] * jump;
+				if (isRanged(nA, nB) && map[nA][nB] == '.') {
+					if (!visited[nA][nB]) {
+						node.a = nA; node.b = nB; node.cnt = cnt + 1;
+						que.push(node);
+					}7
+				}
+				else {
+					break;
+				}
 			}
 		}
 	}
